3584fb30ef9308cac96a69e0b0f7e11e89321fdd
Jerry Gamblin <jerry.gamblin@gmail.com>
2017-09-12 06:51:09 -0500

    Update continuousscan.sh

--- continuousscan.sh
+++ continuousscan.sh
@@ -1,46 +1,61 @@
-#!/bin/bash -u
+#!/bin/bash  
 
- 
-TARGETS="jerrygamblin.com"
-INTERVAL="3600"
-OPTIONS='-sV'
- 
- 
+TARGETS="127.0.0.1"
+OPTIONS='-sV -R'
+
 cd  ~/nmaptable
-LAST_RUN_FILE='.lastrun'
- 
+LAST_RUN_FILE='lastrun'
+
 while true; do
- 
-    # If the last run file exists, we should only sleep for the time
-    # specified minus the time that's already elapsed.
-    if [ -e "${LAST_RUN_FILE}" ]; then
-        LAST_RUN_TS=$(date -r ${LAST_RUN_FILE} +%s)
-        NOW_TS=$(date +%s)
-        LAST_RUN_SECS=$(expr ${NOW_TS} - ${LAST_RUN_TS})
-        SLEEP=$(expr ${INTERVAL} - ${LAST_RUN_SECS})
-        if [ ${SLEEP} -gt 0 ]; then
-            UNTIL_SECS=$(expr ${NOW_TS} + ${SLEEP})
-            echo $(date) "- sleeping until" $(date --date="@${UNTIL_SECS}") "(${SLEEP}) seconds"
-            sleep ${SLEEP}
-        fi
-    fi
- 
-    START_TIME=$(date +%s)
-    echo ''
-    echo '=================='
-    echo ''
+START_TIME=$(date +%s)
+echo ''    
+echo '=================='
+echo '' 
 
-                echo ''
-                echo $(date) "- starting nmap scan"
+DATE=`date +%Y-%m-%d_%H-%M-%S`
 
-        # Remove Old Gnmap
-        rm scan.gnmap
-        # Scan Carfax:
-        nmap ${OPTIONS} ${TARGETS} -oG scan.gnmap > /dev/null
-        #Remove Old Data
-        rm data.csv
-        # Update Data:
-        ./nmapcsv.sh scan.gnmap > data.csv
+echo ''        
+echo $(date) "- Starting Nmap Scan"
+# Remove Old Gnmap
+rm scan.gnmap
+#Start Nmap:
+nmap ${OPTIONS} ${TARGETS} -oG scan.gnmap > /dev/null
+#Remove Old Data
+rm data.csv
+#Update Data:
+./nmapcsv.sh scan.gnmap > data.csv
 
-    done
+
+#2 Hour Count Down Between Scans
+m=${1}-1 
+Floor () {
+DIVIDEND=${1}
+DIVISOR=${2}
+RESULT=$(( ( ${DIVIDEND} - ( ${DIVIDEND} % ${DIVISOR}) )/${DIVISOR} ))
+echo ${RESULT}
+}
+
+Timecount(){
+s=7200
+HOUR=$( Floor ${s} 60/60 )
+s=$((${s}-(60*60*${HOUR})))
+MIN=$( Floor ${s} 60 )
+SEC=$((${s}-60*${MIN}))
+while [ $HOUR -ge 0 ]; do
+while [ $MIN -ge 0 ]; do
+while [ $SEC -ge 0 ]; do
+printf "Rescanning in %02d:%02d:%02d\033[0K\r" $HOUR $MIN $SEC
+SEC=$((SEC-1))
+sleep 1
+done
+SEC=59
+MIN=$((MIN-1))
+done
+MIN=59
+HOUR=$((HOUR-1))
+done
+}
+
+Timecount $m
+
 done
